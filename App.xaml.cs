bd01e0fea621082cc6dcb40bae36f223a082d6f5
Salvador <sarevalotamayo@student.rtc.edu>
2024-04-08 17:59:26 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Prog122Lecturenotes1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
